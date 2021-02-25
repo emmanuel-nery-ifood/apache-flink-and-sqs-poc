74b04ce36cc52ecd48a1bbc6c49518bb26dab2cf
emmanuel.nery <emmanuel.nery@ifood.com.br>
2021-02-25 16:20:34 -0300

    parametrizando o flinksqlconsumer

--- src/main/kotlin/FlinkSqsConsumer.kt
+++ src/main/kotlin/FlinkSqsConsumer.kt
@@ -1,112 +1,189 @@
+//import com.amazon.sqs.javamessaging.ProviderConfiguration
+//import com.amazon.sqs.javamessaging.SQSConnectionFactory
+//import com.amazonaws.auth.profile.ProfileCredentialsProvider
+//import com.amazonaws.client.builder.AwsClientBuilder
+//import com.amazonaws.regions.Regions
+//import com.amazonaws.services.sqs.AmazonSQSClient
+//import org.apache.flink.configuration.Configuration
+//import org.apache.flink.streaming.api.functions.source.RichSourceFunction
+//import org.apache.flink.streaming.api.functions.source.SourceFunction
+//import org.slf4j.LoggerFactory
+//import java.lang.Exception
+//import javax.jms.*
+//import javax.jms.TextMessage
+//
+//
+//class FlinkSqsConsumer :
+//    RichSourceFunction<String>() {
+//    @Volatile
+//    @Transient
+//    private var running = false
+//
+//    @Transient
+//    private lateinit var consumer: MessageConsumer
+//
+//    @Transient
+//    private lateinit var connection: Connection
+//
+//
+//    @Throws(JMSException::class)
+//    private fun init() {
+//        val sqsClient = AmazonSQSClient.builder()
+//            .withCredentials(ProfileCredentialsProvider())
+//            .withEndpointConfiguration(
+//                AwsClientBuilder.EndpointConfiguration(
+//                    "http://localhost:4566",
+//                    Regions.US_EAST_2.toString()))
+//            .build()
+//
+//
+//        val connectionFactory = SQSConnectionFactory(ProviderConfiguration(),
+//            sqsClient)
+//
+//        connection = connectionFactory.createConnection()
+//
+//        connection.start()
+//
+//        val session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)
+//
+//        // Create the destination
+//        val destination = session.createQueue("digester")
+//
+//        consumer = session.createConsumer(destination)
+//
+//        consumer.messageListener = MyListener()
+//    }
+//
+//    @Throws(Exception::class)
+//    override fun open(parameters: Configuration) {
+//        super.open(parameters)
+//        running = true
+//        init()
+//    }
+//
+//    override fun run(ctx: SourceFunction.SourceContext<String>) {
+//        while (running) {
+//            try {
+//
+//                 consumer.receive(1000)?.let {
+//
+//                     val msg = (it as TextMessage).text!!
+//                     ctx.collect(msg)
+//                 }
+//
+//
+//
+//            } catch (e: JMSException) {
+//                LOG.error(e.localizedMessage)
+//                running = false
+//            }
+//        }
+//        try {
+//            close()
+//        } catch (e: Exception) {
+//            LOG.error(e.message, e)
+//        }
+//    }
+//
+//    override fun cancel() {
+//        running = false
+//    }
+//
+//    @Throws(Exception::class)
+//    override fun close() {
+//        LOG.info("Closing")
+//        try {
+//            connection.close()
+//        } catch (e: JMSException) {
+//            throw RuntimeException("Error while closing SQS connection ", e)
+//        }
+//    }
+//
+//    companion object {
+//        private const val serialVersionUID = 1L
+//        private val LOG = LoggerFactory.getLogger(FlinkSqsConsumer::class.java)
+//    }
+//}
+
 import com.amazon.sqs.javamessaging.ProviderConfiguration
 import com.amazon.sqs.javamessaging.SQSConnectionFactory
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
 import com.amazonaws.client.builder.AwsClientBuilder
-import com.amazonaws.regions.Regions
 import com.amazonaws.services.sqs.AmazonSQSClient
+import javax.jms.Connection
+import javax.jms.JMSException
+import javax.jms.MessageConsumer
+import javax.jms.Session.AUTO_ACKNOWLEDGE
+import javax.jms.TextMessage
 import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.functions.source.RichSourceFunction
 import org.apache.flink.streaming.api.functions.source.SourceFunction
 import org.slf4j.LoggerFactory
-import java.lang.Exception
-import javax.jms.*
-import javax.jms.TextMessage
-
-
-class FlinkSqsConsumer :
-    RichSourceFunction<String>() {
-    @Volatile
-    @Transient
+class FlinkSqsConsumer(
+    private val queueName: String,
+    private val endpointUrl: String,
+    private val region: String
+) : RichSourceFunction<String>() {
     private var running = false
-
-    @Transient
     private lateinit var consumer: MessageConsumer
-
-    @Transient
     private lateinit var connection: Connection
-
-
-    @Throws(JMSException::class)
     private fun init() {
+        logger.info("Initializing...")
         val sqsClient = AmazonSQSClient.builder()
             .withCredentials(ProfileCredentialsProvider())
             .withEndpointConfiguration(
                 AwsClientBuilder.EndpointConfiguration(
-                    "http://localhost:4566",
-                    Regions.US_EAST_2.toString()))
+                    endpointUrl,
+                    region
+                )
+            )
             .build()
-
-        // Create a ConnectionFactory
-        val connectionFactory = SQSConnectionFactory(ProviderConfiguration(),
-            sqsClient)
-
-        // Create a Connection
+        val connectionFactory = SQSConnectionFactory(
+            ProviderConfiguration(),
+            sqsClient
+        )
         connection = connectionFactory.createConnection()
-
         connection.start()
-
-        // Create a Session
-        val session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)
-
-        // Create the destination (Topic or Queue)
-        val destination = session.createQueue("digester")
-
-        // Create a MessageConsumer from the Session to the Topic or Queue
+        val session = connection.createSession(false, AUTO_ACKNOWLEDGE)
+        val destination = session.createQueue(queueName)
         consumer = session.createConsumer(destination)
-
     }
-
-    @Throws(Exception::class)
     override fun open(parameters: Configuration) {
         super.open(parameters)
         running = true
         init()
     }
-
     override fun run(ctx: SourceFunction.SourceContext<String>) {
-        // this source never completes
         while (running) {
             try {
-                // Wait for a message
-                 consumer.receive(1000)?.let {
-
-                     val msg = (it as TextMessage).text!!
-//                     it.acknowledge()
-//                    val person = Json.decodeFromString<Person>(msg)
-//                    println("Received: $person")
-                     ctx.collect(msg)
-                 }
-
-
-
+                consumer.receive(1000)?.let {
+                    val msg = (it as TextMessage).text!!
+                    ctx.collect(msg)
+                }
             } catch (e: JMSException) {
-                LOG.error(e.localizedMessage)
+                logger.error(e.localizedMessage)
                 running = false
             }
         }
         try {
             close()
         } catch (e: Exception) {
-            LOG.error(e.message, e)
+            logger.error(e.message, e)
         }
     }
-
     override fun cancel() {
         running = false
     }
-
-    @Throws(Exception::class)
     override fun close() {
-        LOG.info("Closing")
         try {
+            logger.info("Closing...")
             connection.close()
         } catch (e: JMSException) {
-            throw RuntimeException("Error while closing ActiveMQ connection ", e)
+            throw RuntimeException("Error while closing connection", e)
         }
     }
-
     companion object {
         private const val serialVersionUID = 1L
-        private val LOG = LoggerFactory.getLogger(FlinkSqsConsumer::class.java)
+        private val logger = LoggerFactory.getLogger(FlinkSqsConsumer::class.java)
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import com.amazonaws.regions.Region
+import com.amazonaws.regions.Regions
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
@@ -6,7 +8,11 @@ import org.apache.flink.streaming.api.windowing.time.Time
 
 fun main(){
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment()
-    val stream = env.addSource(FlinkSqsConsumer())
+    val stream = env.addSource(FlinkSqsConsumer(
+        queueName = "digester",
+        endpointUrl = "http://localhost:4566",
+        region = Regions.US_EAST_2.toString()
+    ))
 
 
     val personFiltered = stream
